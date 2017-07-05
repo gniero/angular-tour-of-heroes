b23e9554c5f38d89233d4118ba49fb83ef48d898
Guilherme Niero <gniero@gmail.com>
2017-07-04 21:36:11 -0300

    Chapter 5 done

---
+++ src/app/hero.service.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+}
